9013f4a405045d37ae5958a5064c64df312660ee
Rahul RK <47377566+DevTMK@users.noreply.github.com>
2021-04-10 17:25:40 +0530

    My First commit

---
+++ controllers/transactions.js
@@ -0,0 +1,80 @@
+const Transaction = require('../models/Transactions')
+
+// @desc	Get all transactions
+// @route	GET /api/v1/transactions
+// @access	Public
+exports.getTransactions = async (req, res, next) => {
+	try {
+		const transactions = await Transaction.find().sort({ createdAt: -1 })
+
+		return res.status(200).json({
+			success: true,
+			count: transactions.length,
+			data: transactions,
+		})
+	} catch (err) {
+		return res.status(500).json({
+			status: false,
+			error: 'Server Error',
+		})
+	}
+}
+
+// @desc	Add transactions
+// @route	POST /api/v1/transactions
+// @access	Public
+exports.addTransaction = async (req, res, next) => {
+	try {
+		const { text, amount } = req.body
+
+		const transaction = await Transaction.create(req.body)
+
+		return res.status(201).json({
+			success: true,
+			data: transaction,
+		})
+	} catch (err) {
+		console.log(err.name)
+		if (err.name === 'ValidationError') {
+			const messages = Object.values(err.errors).map(val => val.message)
+
+			return res.status(400).json({
+				success: false,
+				error: messages,
+			})
+		} else {
+			return res.status(500).json({
+				status: false,
+				error: 'Server Error',
+			})
+		}
+	}
+}
+
+// @desc	Delete transactions
+// @route	DELETE /api/v1/transactions/:id
+// @access	Public
+exports.deleteTransaction = async (req, res, next) => {
+	try {
+		const transaction = await Transaction.findById(req.params.id)
+
+		if (!transaction) {
+			return res.status(404).json({
+				success: false,
+				error: 'No transaction found',
+			})
+		}
+
+		await transaction.remove()
+
+		return res.status(200).json({
+			success: true,
+			data: {},
+		})
+	} catch (err) {
+		return res.status(500).json({
+			status: false,
+			error: 'Server Error',
+		})
+	}
+}
